17990f7033f63da5d7e552814aea2128e711f551
horsellcommon <crawfordj716@gmail.com>
2022-09-26 18:52:24 +0100

    Problem 6 changed and completed

--- problem6.js
+++ problem6.js
@@ -1,11 +1,28 @@
 /*
-Write a function that takes two numbers, say x and y, as arguments
-Check if x is divisible by y
-If yes, return x
-If not, return the next higher natural number that is divisible by y
+Write a function that takes 6 values (a,b,c,d,e,f) as arguments
+Sum a and b
+Then substract by c
+Then multiply by d and divide by e
+Finally raise to the power of f and return the result
+Tip: mind the order
 */
 
-const divisible = (x, y) => {}
-module.exports = {
-    divisible
+const sixValues = (a, b, c, d, e, f) => {
+  const sum = a+b
+  const subtract = sum-c
+  const multiply = subtract*d
+  const divide = multiply/e
+  const resultingSum = divide**f
+  return resultingSum
+};
+
+sixValues();
+
+const sixValuesImproved = (a, b, c, d, e, f) => {
+  return (((a+b-c) * d) / e) ** f
 }
+
+module.exports = {
+  sixValues,
+  sixValuesImproved
+};
