c6900235c294a3421732a67c14f42fa41e32f7ae
horsellcommon <crawfordj716@gmail.com>
2022-09-26 14:10:36 +0100

    Basic problem/problem.test additions

---
+++ problem6.js
@@ -0,0 +1,11 @@
+/*
+Write a function that takes two numbers, say x and y, as arguments
+Check if x is divisible by y
+If yes, return x
+If not, return the next higher natural number that is divisible by y
+*/
+
+const divisible = (x, y) => {}
+module.exports = {
+    divisible
+}
